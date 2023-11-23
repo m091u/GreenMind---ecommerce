5db03ff20ae91ba82cc7df15b48ca91402fb0f65
Madhushree B <boylemadhushree@gmail.com>
2023-11-23 17:48:38 +0100

    add jwt middleware

---
+++ server/middleware/jwt.middleware.js
@@ -0,0 +1,20 @@
+const {expressjwt: jwt} = require("express-jwt");
+
+// Instantiate the JWT token validation middleware
+const isAuthenticated = jwt({
+    secret: process.env.TOKEN_SECRET,
+    algorithm: ["HS256"],
+    requestProperty:'payload',
+    getToken: getTokenFromHeaders
+});
+
+// Function used to extracts the JWT token from the request's 'Authorization' Headers
+function getTokenFromHeaders(req){
+    if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+        const token = req.headers.authorization.split(" ")[1];
+        return token;
+    }
+    return null
+}
+
+module.exports = {isAuthenticated}
