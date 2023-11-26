27ac757d87ed49867a2f8ac2776fcc0f1729b63e
m091u <mirabelatiugan@gmail.com>
2023-11-26 13:41:21 +0100

    navbar & footer & final folder structure

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+
+function Navbar() {
+  // const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <Link to="/">
+          <button className="nav-button">Home</button>
+        </Link>
+        <Link to="/about">
+          <button className="nav-button">Products</button>
+        </Link>
+      </div>
+      <div className="nav-right">
+        <Link to="/checkout">
+          <button className="nav-button"><i class="fas fa-shopping-basket"></i></button>
+        </Link>
+        <Link to="/login">
+          <button className="nav-button"><i class="fas fa-user"></i></button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
