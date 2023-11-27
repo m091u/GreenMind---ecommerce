5ff7dae0d95b8176287ad4b0c513d3434c7cd00a
m091u <mirabelatiugan@gmail.com>
2023-11-27 10:50:39 +0100

    other

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  console.log("Received request at /");
   res.json("All good in here");
 });
 
-module.exports = router ;
+module.exports = router;
