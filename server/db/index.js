515c5d506c9bc94a6fcd8ab4d6211003ba5fd161
m091u <mirabelatiugan@gmail.com>
2023-12-10 13:52:52 +0100

    switch to atlas server

--- server/db/index.js
+++ server/db/index.js
@@ -6,9 +6,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ecommerce-server";
-// || "mongodb+srv://mirabela_tiugan :ZqMZ0nWXtejPQEe@cluster0.tuqlrtv.mongodb.net/?retryWrites=true&w=majority"
-  //
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://mirabela_tiugan:rinmtuOltJBrB@cluster0.tuqlrtv.mongodb.net/greenmind?retryWrites=true&w=majority'
+  // || "mongodb://127.0.0.1:27017/ecommerce-server";
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
