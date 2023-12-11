2be393b8b741e295349e330e8eaab10c255821b6
Madhushree B <boylemadhushree@gmail.com>
2023-12-11 11:58:20 +0100

    minor changes in server

--- server/db/index.js
+++ server/db/index.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecommerce-server";
+  process.env.MONGODB_URI ||  
+  // || "mongodb://127.0.0.1:27017/ecommerce-server";
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
