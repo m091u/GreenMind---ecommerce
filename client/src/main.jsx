52a0985e261e467bf272da61b6c87f8e66499fe1
Madhushree B <boylemadhushree@gmail.com>
2024-01-08 17:38:11 +0100

    changes in the carousel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // import '@fortawesome/fontawesome-free/css/all.min.css';
 
